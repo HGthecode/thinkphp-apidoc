ac8ae510e3feb701ab63033d62f5c977a169a937
hg-code <376401263@qq.com>
2021-08-26 17:14:35 +0800

    Update config.php

--- src/config.php
+++ src/config.php
@@ -1,62 +1,51 @@
 <?php
 return [
     // 文档标题
-    'title'              => 'APi接口文档',
+    'title'              => 'API接口文档',
     // 文档描述
     'desc'               => '',
-    // 版权申明
-    'copyright'          => 'Powered By hg-code',
-    // 默认作者
-    'default_author'=>'',
     // 默认请求类型
     'default_method'=>'GET',
-    // 设置应用/版本（必须设置）
-    'apps'           => [
-        ['title'=>'v1.0','path'=>'app\controller','folder'=>'v1'],
+    // 允许跨域访问
+    'allowCrossDomain'=>false,
+    // 设置可选版本
+    'apps'           => [],
+    // 自动生成url规则
+    'auto_url' => [
+        // 字母规则
+        'letter_rule' => "lcfirst",
+        // 多级路由分隔符
+        'multistage_route_separator'  =>"."
     ],
-    // 控制器分组
-    'groups'             => [],
     // 指定公共注释定义的文件地址
-    'definitions'        => "app\controller\Definitions",
-    //指定生成文档的控制器
-    'controllers'        => [],
-    // 过滤，不解析的控制器
-    'filter_controllers' => [],
+    'definitions'        => "app\common\controller\Definitions",
     // 缓存配置
     'cache'              => [
         // 是否开启缓存
         'enable' => false,
-        // 缓存文件路径
-        'path'   =>  '../runtime/apidoc/',
-        // 是否显示更新缓存按钮
-        'reload' => true,
-        // 最大缓存文件数
-        'max'    => 5,  //最大缓存数量
     ],
     // 权限认证配置
     'auth'               => [
         // 是否启用密码验证
         'enable'     => false,
-        // 验证密码
+        // 全局访问密码
         'password'   => "123456",
         // 密码加密盐
         'secret_key' => "apidoc#hg_code",
+        // 有效期
+        'expire' => 24*60*60
     ],
     // 统一的请求Header
     'headers'=>[],
     // 统一的请求参数Parameters
     'parameters'=>[],
-    // 统一的请求响应体，仅显示在文档提示中
+    // 统一的请求响应体
     'responses'=>[
-        ['name'=>'code','desc'=>'状态码','type'=>'int'],
-        ['name'=>'message','desc'=>'操作描述','type'=>'string'],
+        ['name'=>'code','desc'=>'代码','type'=>'int'],
+        ['name'=>'message','desc'=>'业务信息','type'=>'string'],
         ['name'=>'data','desc'=>'业务数据','main'=>true,'type'=>'object'],
     ],
     // md文档
-    'docs'              => [
-        'menu_title' => '开发文档',
-        'menus'      => []
-    ],
-    'crud'=>[]
+    'docs'              => [],
 
 ];
